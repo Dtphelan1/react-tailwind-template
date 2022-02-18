d23c7383aa43c140196a30e089d662a01f4ccebd
Dylan Phelan <dtphelan1@gmail.com>
2022-02-17 19:37:28 -0500

    initalize template

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    // Recognize Jest global variables like `test` and `expect`
+    'jest/globals': true,
+  },
+  extends: ['eslint:recommended', 'airbnb', 'airbnb/hooks', 'plugin:react/recommended', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    // allow jsx syntax in js files
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
